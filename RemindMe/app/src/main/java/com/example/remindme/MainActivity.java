0f70724e962bf93718b3d02470626193ee88e7ec
Jimmy <jimmyyo12@gmail.com>
2020-06-12 16:39:51 +1200

    Added drawables + RecyclerView

--- RemindMe/app/src/main/java/com/example/remindme/MainActivity.java
+++ RemindMe/app/src/main/java/com/example/remindme/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Arraylist for location to show user
+        ArrayList<LocationItem> locations = new ArrayList<>();
+        locations.add(new LocationItem(R.drawable.ic_house,"cool1","cool2"));
     }
 }
